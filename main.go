afcadc017dd5f1965c5c3d92649ba0eef55060cb
Nat Welch <nat@natwelch.com>
2023-01-06 02:23:40 +0000

    just brain dumping

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
 func main() {
+	for _, file := range os.Args {
+		uri, err := uploadFile(context.Background(), file)
+		if err != nil {
+			log.Errorf("could not upload file %q: %+v", file, err)
+			os.Exit(1)
+		}
+
+		fmt.Println(uri)
+	}
+}
+
+func uploadFile(ctx context.Context, filePath string) (string, error) {
+	buf := filePath
+	resp, err := http.Post("https://graphql.natwelch.com/photo/new", "image/jpeg", &buf)
+
+	return "", err
 }
